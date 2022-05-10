8e74dac7dcdc7852df8d3aea59677d545750cee8
Caroline Sexton <sissexton@gmail.com>
2022-05-10 15:21:05 -0700

    added clear button

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
+const textarea = document.getElementById("questions");
+//clear textarea value
+textarea.value = "";
+//clear textarea value on click
 
+const clearBtn = document.getElementById("clearBtn");
+
+clearBtn.addEventListener("click", function handleClick() {
+   textarea.value = "";
+});
 //define magic 8 ball answers
 
 let answers = [  
@@ -13,19 +22,20 @@ let answers = [
     "Ask your Mom.",
     "You betcha!"
 ];
-//intro to user
 
     const answerBtn = document.getElementById("answerBtn");
     let answer = document.getElementById("answer");
-    let questionArea = document.getElementById("questionArea");
+    let questions = document.getElementById("questions");
+
 //button with random answer
     answerBtn.addEventListener("click", function() {
-        if (questionArea.value.length < 4) {
+        if (questions.value.length < 4) {
             alert("Please enter your question in the box below.");
         } else {
             answer.innerText = "";
             let num = Math.floor(Math.random() * Math.floor(answers.length));
             answer.innerText = answers[num];
+        
         }
 
     });
@@ -34,7 +44,7 @@ let answers = [
 
 
 
-
+    
 
 
 
